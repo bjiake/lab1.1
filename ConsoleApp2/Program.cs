d20a2fd05b03eb080ec84642bd0f96cdccad1bca
student <student@WS2220-07>
2022-02-07 10:27:57 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = a;
+            for (int i = 0; i < n; ++i)
+            {
+                result = Convert.ToInt32(result * a);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
