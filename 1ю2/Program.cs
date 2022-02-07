8565c9aafac7b9e6c028e7b425c4c3b7b4294343
bjiake <suyd@list.ru>
2022-02-07 11:00:18 +0700

    Добавьте файлы проекта.

---
+++ 1ю2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1ю2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            if(x < 10)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else
+            {
+                string s1 = x.ToString();
+                char s2 = s1[1];
+                s1 = s1.Remove(1, 1);
+                s1 = s1 + s2;
+                Console.WriteLine(s1);
+                
+                
+            }
+        }
+    }
+}
